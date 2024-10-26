4e5f8095b1407b1ae305e5c11d184375c1bada26
Prathamk07 <prathamkalra.dev@gmail.com>
2024-10-26 19:13:28 +0530

    Navbar : Add fonts to Navbar using Tailwindcss

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,13 +5,13 @@ import telelogo from '../../assets/telelogo.svg'
 
 const Navbar = () => {
   return (
-    <div className=' py-6 mt-7 w-[90%] mx-auto rounded-full px-16 text-white bg-black shadow-md shadow-white border border-1 border-gray-400 backdrop-filter backdrop-blur-md bg-opacity-30 fixed right-0 left-0'>
+    <div className='font-[FixelVariable] py-6 mt-7 w-[90%] mx-auto rounded-full px-16 text-white bg-black shadow-md shadow-white border border-1 border-gray-400 backdrop-filter backdrop-blur-md bg-opacity-30 fixed right-0 left-0'>
 
         <div className='flex font-semibold justify-between text-lg'>
             <div className='space-x-20'>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/'}>Downloads</Link>
-                <Link to={'/'}>About</Link>
+                <Link to={'/'}>home</Link>
+                <Link to={'/'}>downloads</Link>
+                <Link to={'/'}>about</Link>
             </div>
             <div className='flex gap-8'>
             <img src={ghlogo} alt="github" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+        // customFont: ['"FixelVariable"', "sans-serif"],
+        // Add more custom font families as needed
+      // },
+    },
   },
   plugins: [],
 }
