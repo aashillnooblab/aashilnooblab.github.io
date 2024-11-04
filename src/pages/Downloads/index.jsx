0b51b39ffca5303977587403feb969ea50cbc02e
Prathamk07 <prathamkalra.dev@gmail.com>
2024-11-04 18:26:40 +0530

    Downloads : Add cards for device list

--- src/pages/Downloads/index.jsx
+++ src/pages/Downloads/index.jsx
@@ -2,7 +2,134 @@ import React from 'react'
 
 const Downloads = () => {
   return (
-    <div>Downloads</div>
+    <div>
+      <div className='min-h-screen bg-custom-radial'>
+        <div className='grid pt-40 pb-40 font-[FixelLight] w-4/5 mx-auto grid-cols-3'>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+          <div className='bg-opacity-40 bg-slate-900 text-white w-4/5 flex p-4 rounded-xl my-2'>
+          <div>
+            <img src="https://github.com/Project-PixelStar/official_devices/blob/14/images/veux.png?raw=true" alt="" />
+            <div className='flex justify-between mt-5'>
+            <span className='my-auto text-md text-slate-300'>Device</span>
+            <span className='text-md rounded-full px-3 text-opacity-100 text-white bg-opacity-90 bg-black'>veux</span>
+            </div>
+            <h3 className='text-2xl font-[FixelBold]'>Redmi Note 11 Pro + 5G</h3>
+            <div className='mt-2'>
+              <span className='my-auto text-md text-slate-300'>Maintainer</span>
+              <br />
+            <h7 className='text-xl'>Prathamk07</h7>
+            </div>
+
+            <div className='w-4/5 my-4 mx-auto'>
+            <button className='w-full mx-auto bg-opacity-40 bg-black px-4 py-2 rounded-xl'>Download</button>
+            </div>
+
+          </div>
+          </div>
+        
+
+        </div>
+      </div>
+    </div>
   )
 }
 
